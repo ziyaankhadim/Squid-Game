53b928aa08a9844b0f47944670a8e691681b5611
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-07-22 12:25:49 +0500

    add basic scene

---
+++ src/script.js
@@ -0,0 +1,108 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/Orbitcontrols.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import gsap from "gsap";
+import * as dat from "dat.gui";
+
+const parameters = {
+  color: 0xffff00,
+  spin: () => {
+    gsap.to(mesh.rotation, 1, { x: mesh.rotation.x + Math.PI * 2 });
+  },
+};
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  //Update Sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //Update Camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  //Update Renderer
+  renderer.setSize(sizes.width, sizes.height);
+  //renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.x = 2;
+camera.position.y = 2;
+camera.position.z = 2;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+//Orbit Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enable = false;
+controls.enableDamping = true;
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+//renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// Animate
+const clock = new THREE.Clock();
+//console.log(clock);
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI({
+  // closed: true,
+  width: 400,
+});
+// gui.hide()
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+gui.add(mesh, "visible");
+gui.add(material, "wireframe");
+
+gui.addColor(parameters, "color").onChange(() => {
+  material.color.set(parameters.color);
+});
+
+gui.add(parameters, "spin");
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  //console.log(elapsedTime);
+
+  // Update objects
+  mesh.rotation.y = elapsedTime;
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+
+  // required if controls.enableDamping or controls.autoRotate are set to true
+  controls.update();
+};
+
+tick();
