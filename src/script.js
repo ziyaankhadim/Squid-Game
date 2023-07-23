718266d714718b8eb76a4f87aeca1672ec4c1f9e
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-07-24 04:53:39 +0500

    remove cube and load doll model to the scene

--- src/script.js
+++ src/script.js
@@ -5,12 +5,12 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import gsap from "gsap";
 import * as dat from "dat.gui";
 
-const parameters = {
-  color: 0xffff00,
-  spin: () => {
-    gsap.to(mesh.rotation, 1, { x: mesh.rotation.x + Math.PI * 2 });
-  },
-};
+// const parameters = {
+//   color: 0xffff00,
+//   spin: () => {
+//     gsap.to(mesh.rotation, 1, { x: mesh.rotation.x + Math.PI * 2 });
+//   },
+// };
 
 /**
  * Base
@@ -46,23 +46,69 @@ const gridHelper = new THREE.GridHelper(100);
 scene.add(gridHelper);
 
 // Object
-const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: parameters.color });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: parameters.color });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
+// Instantiate a loader
+const gltfLoader = new GLTFLoader();
+
+// Load a glTF resource
+gltfLoader.load(
+  // resource URL
+  "/doll/scene.gltf",
+  // called when the resource is loaded
+  function (gltf) {
+    console.log(gltf);
+    gltf.scene.scale.set(1, 1, 1);
+    gltf.scene.position.set(0, 4.75, -45);
+    scene.add(gltf.scene);
+  },
+  //called while loading is progressing
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  //called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+//Lights
+const light = new THREE.AmbientLight(0xffffff); // soft white light
+scene.add(light);
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
-camera.lookAt(mesh.position);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 15;
+//camera.lookAt(mesh.position);
 scene.add(camera);
 
 //Orbit Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enable = false;
 controls.enableDamping = true;
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI({
+  // closed: true,
+  width: 400,
+});
+// gui.hide()
+//gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+//gui.add(mesh, "visible");
+//gui.add(material, "wireframe");
+
+// gui.addColor(parameters, "color").onChange(() => {
+//   material.color.set(parameters.color);
+// });
+
+// gui.add(parameters, "spin");
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -73,31 +119,12 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // Animate
 const clock = new THREE.Clock();
 //console.log(clock);
-
-/**
- * Debug
- */
-const gui = new dat.GUI({
-  // closed: true,
-  width: 400,
-});
-// gui.hide()
-gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
-gui.add(mesh, "visible");
-gui.add(material, "wireframe");
-
-gui.addColor(parameters, "color").onChange(() => {
-  material.color.set(parameters.color);
-});
-
-gui.add(parameters, "spin");
-
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
   //console.log(elapsedTime);
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  //mesh.rotation.y = elapsedTime;
 
   // Render
   renderer.render(scene, camera);
